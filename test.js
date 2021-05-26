0fa24f81434889256f53bee213f43346ca815644
SteveO <steve.gitprofile@outlook.com>
2021-05-26 13:20:59 +0200

    removed test.js

--- test.js
+++
@@ -1,3 +0,0 @@
-const prompt = require('prompt-sync')();
-let userChoice = Number(prompt('\nEnter 1 to get a Menu\nEnter 2 to add a Menu Item\nEnter 3 to exit R U Hungry '));
-console.log(typeof userChoice);
